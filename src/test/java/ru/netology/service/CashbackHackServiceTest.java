52928ef3b46fdf036909b31858243d8fa4535779
Ufimskii <91329112+Ufimskii@users.noreply.github.com>
2022-01-08 16:52:39 +0500

    AT_1_1_testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCalculateIfAmountCorrect() {
+        int amount = 5700;
+        int actual = service.remain(amount);
+        int expected = 300;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountInCorrect() {
+        int amount = 5000;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+}
